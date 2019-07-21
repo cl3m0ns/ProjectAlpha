ea17ecc56895d3f29310fc4f05084f5ebf352e53
Kyle Clemons <cl3m0ns@gmail.com>
2019-07-20 22:47:59 -0400

    added states for player

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -5,9 +5,59 @@ export (int) var speed = 75
 
 var velocity = Vector2()
 var lastVelocity = Vector2(0,0)
-var lastAnimation
+var lastAnimation = "Idle"
 
-func get_inputs():
+const facing = {
+	left = "left",
+	right = "right",
+	up = "up",
+	down = "down"
+}
+
+const states = {
+	idle="Idle",
+	walk="Walk",
+	attack="Attack" 
+}
+
+var STATE = "";
+var PREV_STATE = "";
+var NEXT_STATE = states.idle;
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	idle_state()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	STATE = NEXT_STATE
+	get_inputs()
+	
+	if STATE == states.idle:
+		idle_state()
+	elif STATE == states.walk:
+		walk_state()
+	elif STATE == states.attack:
+		attack_state()
+
+func idle_state():
+	$AnimationPlayer.play("Idle");
+	set_last_animation_and_velocity()
+
+func walk_state():
+	get_movement()
+	update_sprite()
+	move_and_slide(velocity)
+
+func attack_state():
+	set_last_animation_and_velocity()
+	pass
+
+func set_last_animation_and_velocity():
+	lastAnimation = $AnimationPlayer.get_current_animation()
+	lastVelocity = velocity;
+
+func get_movement():
 	var tempVelocity = Vector2()
 	if Input.is_action_pressed('right'):
 		tempVelocity.x += 1
@@ -19,17 +69,21 @@ func get_inputs():
 		tempVelocity.y -= 1
 	velocity = tempVelocity.normalized() * speed
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	$AnimationPlayer.play("Idle");
-	lastAnimation = $AnimationPlayer.get_current_animation()
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta):
-	get_inputs()
-	update_sprite()
-	move_and_slide(velocity)
+func get_inputs():
+	var right = Input.is_action_pressed("right")
+	var left = Input.is_action_pressed("left")
+	var up = Input.is_action_pressed("up")
+	var down = Input.is_action_pressed("down")
+	var attack = Input.is_action_pressed("attack")
+	
+	if attack:
+		NEXT_STATE = states.attack
+	elif right || left || up || down:
+		NEXT_STATE = states.walk
+	else:
+		NEXT_STATE = states.idle
+	
+	PREV_STATE = STATE
 
 func update_sprite():
 	var movingHorizontal = false
@@ -48,9 +102,9 @@ func update_sprite():
 	elif velocity.y < 0 && !movingHorizontal:
 		$AnimationPlayer.play("Walk Up")
 	elif velocity.y == 0 && velocity.x == 0:
-		$AnimationPlayer.play("Idle")
+		NEXT_STATE = states.idle
+		PREV_STATE = STATE
 	else:
 		$AnimationPlayer.play(lastAnimation)
 	
-	lastAnimation = $AnimationPlayer.get_current_animation()
-	lastVelocity = velocity;
+	set_last_animation_and_velocity()
